233dd33a7c3984a56f48b76f6b5f4e91eeb2bb27
Connor Johnston <cgjohn@umich.edu>
2018-09-24 16:33:17 -0400

    added SCSS support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import classes from './App.css';
+import classes from './App.scss';
 
 import Open from './Open/Open'
 import Home from './Home/Home'
---
+++ src/ExpandableSection/Section.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import classes from './Section.css';
+
+
+class Section extends Component {
+  render() {
+
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Section;
